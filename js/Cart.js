9e0b87b000812ac2ae4ddc0eccbd2114a92abbbf
jaiswal-sakshi <jaiswalsakshi292004@gmail.com>
2025-04-07 20:27:03 +0530

    cart updated

--- js/Cart.js
+++ js/Cart.js
@@ -1,75 +1,59 @@
-var Cart = JSON.parse(localStorage.getItem("Cart"));
+var Cart = JSON.parse(localStorage.getItem("Cart")) || [];
 
-/* footer top showing total lenth */
-var totalItem = Cart.length;
-if (totalItem==null){
-  document.getElementById("total-item").innerText = "0";
-  // alert("no item in cart")
-}
-else{
-  document.getElementById("total-item").innerText = totalItem;
-}
+document.getElementById("total-item").innerText = Cart.length;
 
-var totalPrice = Cart.reduce(function (sum, ele, ind, arr) {
+var totalPrice = Cart.reduce(function (sum, ele) {
   return sum + Number(ele.price);
 }, 0);
 document.getElementById("Total").innerText = totalPrice;
 
-// mapping a function
-
 Cart.map(function (ele, index) {
   var row = document.createElement("tr");
+
   var td1 = document.createElement("td");
-
   var img = document.createElement("img");
-  img.setAttribute = ("class", "cardsImg");
+  img.classList.add("cardsImg");
   img.src = ele.img;
-
+  img.alt = ele.name;
   td1.append(img);
 
   var td2 = document.createElement("td");
-  td2.innerHTML = ele.name;
+  td2.innerText = ele.name;
 
   var td3 = document.createElement("td");
-  td3.innerHTML = ele.price;
+  td3.innerText = ele.price;
 
   var td4 = document.createElement("td");
-  td4.innerHTML=1 
+  td4.innerText = 1;
 
+  var td5 = document.createElement("td");
   var icon = document.createElement("i");
   icon.classList.add("fa", "fa-trash");
   icon.addEventListener("click", function () {
-    removeItem(ele, index);
+    removeItem(index);
   });
-  var td5 = document.createElement("td");
   td5.append(icon);
 
   row.append(td1, td2, td3, td4, td5);
   document.querySelector("tbody").append(row);
 });
 
-// function to add to Cart
 function addCart(ele) {
   Cart.push(ele);
   localStorage.setItem("Cart", JSON.stringify(Cart));
 }
 
-
-// removing item from Cart
-
-function removeItem(ele, index) {
+function removeItem(index) {
   Cart.splice(index, 1);
   localStorage.setItem("Cart", JSON.stringify(Cart));
-  window.location.reload();
+  window.location.reload(); // Or re-render the cart dynamically
 }
-
 
 var paymentBtn = document.getElementById("payment");
 paymentBtn.addEventListener("click", proceedToPay);
 
-var PaymentGateway = JSON.parse(localStorage.getItem("PaymentGateway"));
 function proceedToPay() {
+  console.log(Cart);
   localStorage.setItem("PaymentGateway", JSON.stringify(Cart));
   localStorage.removeItem("Cart");
-  console.log(Cart);
 }
